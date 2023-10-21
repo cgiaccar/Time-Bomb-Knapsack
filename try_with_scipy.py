f5ac0fa05e9ce8ef4483039edd6830eb575725fc
Camilla Giaccari <cami.lla@me.com>
2023-10-21 20:24:43 +0200

    SciPy example is now the same of the gurobi example (#7)
    They both give the same result, so it's correct

--- try_with_scipy.py
+++ try_with_scipy.py
@@ -10,8 +10,8 @@ import numpy as np
 from scipy import optimize
 from scipy.optimize import milp
 
-w = [4, 2, 5, 4, 5, 1, 3, 5]  # weight
-p = [10, 5, 18, 12, 15, 1, 2, 8]  # profit
+w = np.array([4, 2, 5, 4, 5, 1, 3, 5])  # weight
+p = np.array([10, 5, 18, 12, 15, 1, 2, 8])  # profit
 q = [0, 0, 0.2, 0.5, 0.8, 0.1, 0, 0.7]  # probability of exploding
 pi = [1-i for i in q]  # probability of NOT exploding
 c = 15  # capacity
@@ -19,14 +19,13 @@ n = len(w)  # number of items
 
 T = [pi.index(j) for j in pi if j < 1]  # set of time-bomb items
 
-sizes = np.array([21, 11, 15, 9, 34, 25, 41, 52])
-values = np.array([22, 12, 16, 10, 35, 26, 42, 53])
+
 bounds = optimize.Bounds(0, 1)  # 0 <= x_i <= 1
-integrality = np.full_like(values, True)  # x_i are integers
-capacity = 100
-constraints = optimize.LinearConstraint(A=sizes, lb=0, ub=capacity)
-res = milp(c=-values, constraints=constraints,
+integrality = np.full_like(p, True)  # x_i are integers
+constraints = optimize.LinearConstraint(A=w, lb=0, ub=c)
+res = milp(c=-p, constraints=constraints,
            integrality=integrality, bounds=bounds)
 
-print(res.success)  # -> True
-print(res.x)    # -> array([1., 1., 0., 1., 1., 1., 0., 0.])
+print(res.success)
+print(res.fun)
+print(res.x)
