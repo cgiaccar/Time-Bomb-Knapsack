f44eae6806fa5cd2aaea313099a02e964c67388d
Camilla Giaccari <cami.lla@me.com>
2023-11-27 12:10:28 +0100

    Cleaned gekko solver (#10)

--- try_with_gekko.py
+++ try_with_gekko.py
@@ -1,41 +1,30 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 11 19:16:22 2023
-
-!!!! IT'S WORKING !!!!
-
-@author: Camilla
-"""
-
-###################### FULL TRY ##########################
-
 import numpy as np
 from gekko import GEKKO
 
-# simple example to do irl
-# w = [8, 5, 10]
-# p = [4, 2, 5]
-# q = [0.1, 0.9, 0]
-# q = [0, 0, 0]
+# simple example
+# w = [8, 5, 10]  # weight
+# p = [4, 2, 5]  # profit
+# q = [0.1, 0.9, 0]  # probability of exploding
+# q = [0, 0, 0]  # all zeros --> standard knapsack
+# c = 15
 
-# another example for funsies
-w = [23, 10, 15, 35, 20, 60, 52, 16, 17, 28]
-p = [30, 5, 43, 17, 20, 100, 42, 24, 13, 300]
-q = [0.5, 0, 0.9, 0, 0.2, 0.6, 0.4, 0.3, 0, 1]
-# q = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# another example
+w = [23, 10, 15, 35, 20, 60, 52, 16, 17, 28]  # weight
+p = [30, 5, 43, 17, 20, 100, 42, 24, 13, 300]  # profit
+q = [0.5, 0, 0.9, 0, 0.2, 0.6, 0.4, 0.3, 0, 1]  # probability of exploding
+# q = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # all zeros --> standard knapsack
 c = 77
 
+# used example
 # w = [4, 2, 5, 4, 5, 1, 3, 5]  # weight
 # p = [10, 5, 18, 12, 15, 1, 2, 8]  # profit
 # q = [0, 0, 0.2, 0.5, 0.8, 0.1, 0, 0.7]  # probability of exploding
 # q = [0, 0, 0, 0, 0, 0, 0, 0]  # all zeros --> standard knapsack
-pi = [1-i for i in q]  # probability of NOT exploding
 # c = 15  # capacity
-n = len(w)  # number of items
 
-# T = [pi.index(j) for j in pi if j < 1]  # set of time-bomb items
-T = [i for i in range(len(pi)) if pi[i] < 1]
+pi = [1-i for i in q]  # probability of NOT exploding
+n = len(w)  # number of items
+T = [i for i in range(len(pi)) if pi[i] < 1]  # set of time-bomb items
 
 
 m = GEKKO(remote=False)  # create GEKKO model
@@ -52,109 +41,3 @@ m.Maximize(sum(p[i]*x[i] for i in range(n)) *
 
 m.solve(disp=True)
 print(x)  # print solution
-
-######################################################################
-
-
-###################### SIMPLE KNAPSACK ##########################
-
-# import numpy as np
-# from gekko import GEKKO
-
-# w = [4, 2, 5, 4, 5, 1, 3, 5]  # weight
-# p = [10, 5, 18, 12, 15, 1, 2, 8]  # profit
-# c = 15  # capacity
-# n = len(w)  # number of items
-
-# m = GEKKO(remote=False)  # create GEKKO model
-
-# x = [m.Var(lb=0, ub=1, integer=True) for j in range(n)]
-# m.Equations([sum(w[j]*x[j] for j in range(n)) <= c])
-# m.Maximize(sum(p[j]*x[j] for j in range(n)))
-
-# m.solve(disp=True)
-# print(x)  # print solution
-
-######################################################################
-
-
-###################### SIMPLE FOUND ONLINE ##########################
-
-# from gekko import GEKKO
-# m = GEKKO(remote=False)  # create GEKKO model
-# x = m.Var()            # define new variable, default=0
-# y = m.Var()            # define new variable, default=0
-# m.Equations([3*x+2*y == 1, x+2*y == 0])  # equations
-# m.solve(disp=False)    # solve
-# print(x.value, y.value)  # print solution
-
-######################################################################
-
-
-###################### TUTOR WITH PAPER ##########################
-
-# import random
-# import numpy as np
-# from gekko import GEKKO
-# np.random.seed(0)
-# random.seed(0)
-
-# N = 10
-# frac = 2
-# T = np.random.choice(range(N), N//frac)
-
-# print(T)
-
-# q = np.random.uniform(0, 0.99999, size=N)  # il prof aveva scritto size=len(T)
-# q /= q.sum()
-# p = np.random.uniform(0, 10, size=N)
-
-# w = np.random.uniform(0, 0.99999, size=N)
-# c = w.sum()/4
-
-
-# m = GEKKO(remote=False)
-
-# x = [m.Var(lb=0, ub=1, integer=True) for i in range(N)]
-
-# # m.Equation(==Demand)
-# m.Obj(-(m.sum([p[j]*x[j] for j in range(N)]))
-#       * (np.prod([1 - q[i]*x[i] for i in T])))
-# m.options.SOLVER = 1
-
-# m.solve()
-# print(x)
-
-##################################################################
-
-
-###################### TUTOR FOUND ONLINE ##########################
-
-# https://www.gurobi.com/documentation/current/refman/py_model_agc_poly.html
-
-# import numpy as np
-# from gekko import GEKKO
-
-# N = 10
-# T = [0, 1, 2, 3, 4]
-
-# q = np.random.uniform(0, 0.99999, size=len(T))
-# q /= q.sum()
-# p = np.random.uniform(0, 10, size=N)
-
-# w = np.random.uniform(0, 0.99999, size=N)
-# c = w.sum()/4
-
-
-# m = GEKKO(remote=False)
-
-# x = [m.Var(lb=0, ub=1, integer=True) for i in range(N)]
-
-# # m.Equation(==Demand)
-# m.Obj(-(m.sum([p[j]*x[j] for j in range(N)]))
-#       * (np.prod([1 - q[i]*x[i] for i in T])))
-# m.options.SOLVER = 1
-# m.solve()
-# print(x)
-
-#######################################################################
