d2fff67a2f24ca2907fecce0c77a7a0b73257679
Camilla Giaccari <cami.lla@me.com>
2023-12-11 18:07:35 +0100

    Undid last commit
    First try at the actual data gives lots of time problems (#13)

--- Parallel_subset_enum_alg.py
+++ Parallel_subset_enum_alg.py
@@ -98,92 +98,12 @@ if __name__ == '__main__':
 
     # piece of code taken from the main
 
-    # # another example
-    # w = [23, 10, 15, 35, 20, 60, 52, 16, 17, 28]  # weight
-    # p = [30, 5, 43, 17, 20, 100, 42, 24, 13, 300]  # profit
-    # q = [0.5, 0, 0.9, 0, 0.2, 0.6, 0.4, 0.3, 0, 1]  # probability of exploding
-    # # q = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # all zeros --> standard knapsack
-    # c = 77
-
-    """
-    Code to load all the files into dataframes
-    This will create three dictionaries with the name of the file as key and value:
-        its n in ns,
-        its capacity in capacities
-        and its corresponding dataframe in dataframes.
-
-    """
-
-    import glob
-    import pandas as pd
-
-    # Import as a df the first txt file
-    # file = 'generated-instances/type1-100-0-0.1-1.txt'
-    # cols = ['weight', 'profit', 'probability']
-    # dataframe = pd.read_csv(file, sep=r"\s+", names=cols, skiprows=1,header=None)
-    # header = pd.read_csv(file, nrows=0,  usecols=[0])
-    # header = header.columns.values[0].split()
-    # n = header[0]
-    # capacity = header[1]
-    # print(f"{n = }")
-    # print(f"{capacity = }")
-    # print(dataframe)
-
-    # Now import everything
-
-    # All files and directories ending with .txt and that don't begin with a dot:
-    filenames = glob.glob("Data/generated-instances/type1-100-0-0.1-1.txt")
-
-    # empty dictionaries to store n, capacity and dataframe from each file
-    ns = dict()
-    capacities = dict()
-    dataframes = dict()
-    cols = ['weight', 'profit', 'probability']
-
-    # iterate to fill the dictionaries
-    for file in filenames:
-        dataframe = pd.read_csv(file, sep=r"\s+", names=cols,
-                                skiprows=1, header=None)
-        header = pd.read_csv(file, nrows=0,  usecols=[0])
-        header = header.columns.values[0].split()
-        ns[file] = header[0]
-        capacities[file] = header[1]
-        dataframes[file] = dataframe
-
-    # test print to check if it worked
-    test_file = "Data/generated-instances/type1-100-0-0.1-1.txt"
-    # print(f"{test_file = }")
-    # print(f"n = {ns[test_file]}")
-    # print(f"capacity = {capacities[test_file]}")
-    # print(dataframes[test_file].head(3))  # first three rows
-
-    w = dataframes[test_file]['weight'].tolist()
-    p = dataframes[test_file]['profit'].tolist()
-    q = dataframes[test_file]['probability'].tolist()
-    c = int(capacities[test_file])
-
-    # [Done] exited with code=null in 43.44 seconds
-    # w = w[:len(w)//2]
-    # p = p[:len(p)//2]
-    # q = q[:len(q)//2]
-
-    # [Done] exited with code=null in 36.296 seconds
-    # w = w[:len(w)//3]
-    # p = p[:len(p)//3]
-    # q = q[:len(q)//3]
-
-    # Memoria computer esaurita ([Done] exited with code=null in 81.176 seconds)
-    w = w[:len(w)//4]
-    p = p[:len(p)//4]
-    q = q[:len(q)//4]
-
-    print(len(w))
-    print(len(p))
-    print(len(q))
-
-    print(type(c))
-    print(type(w[0]))
-
+    # another example
+    w = [23, 10, 15, 35, 20, 60, 52, 16, 17, 28]  # weight
+    p = [30, 5, 43, 17, 20, 100, 42, 24, 13, 300]  # profit
+    q = [0.5, 0, 0.9, 0, 0.2, 0.6, 0.4, 0.3, 0, 1]  # probability of exploding
+    # q = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # all zeros --> standard knapsack
+    c = 77
     par_x, par_obj, par_time = ParTBEnum(w, p, c, q)
     print(
         f"\nParallel Subset Enumeration algorithm solution:\nx = {par_x} \nobj = {par_obj} \ntime = {par_time:0.6f}")
